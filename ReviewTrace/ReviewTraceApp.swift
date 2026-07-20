2bb12bb75a2f564c82b318040e46ea2a4c98d150
woozy-A <274724396+woozy-A@users.noreply.github.com>
2026-07-21 02:22:47 +0900

    chore: capture fresh Xcode project baseline

---
+++ ReviewTrace/ReviewTraceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReviewTraceApp.swift
+//  ReviewTrace
+//
+//  Created by ycw012 on 7/21/26.
+//
+
+import SwiftUI
+
+@main
+struct ReviewTraceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
